a6fe28740aedc3835d2661922d2e9ab1553a1e1b
mohmed-ahmed-01097 <mohmedahmed01097@gmail.com>
2023-02-27 01:02:08 +0200

    Number System Conversiong Project

---
+++ 06 - Review/02 - Projects/01 - Conversion Between Two Numbers Systems/ConvertNumberSystem.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "ConvertNumberSystem.h"
+
+#define MAX_LENGTH     19
+
+void main(void){
+    long long int number;
+    int source, destination;
+
+    printf("Enter a source number system:");
+    fflush(stdout); fflush(stdin);
+    scanf("%i", &source);
+    printf("Enter a destination number system:");
+    fflush(stdout); fflush(stdin);
+    scanf("%i", &destination);
+    printf("Enter a number Number max 19-bit:");
+    fflush(stdout); fflush(stdin);
+    scanf("%lli", &number);
+
+    printf("%lli in [%i sys] = %lli in [%i sys]"
+           , number, source, ConvertNumSys(source, destination, number), destination);
+}
+
+long long int ConvertNumSys(int src, int dis, long long int num){
+    return ConvertFromDecimal(dis, ConvertToDecimal(src, num));
+}
+
+long long int ConvertToDecimal(int src, long long int num){
+    long long int ret = 0;
+    int pow = 1;
+    while(num){
+        ret += (num % 10) * pow;
+        pow *= src;
+        num /= 10;
+    }
+    return ret;
+}
+
+long long int ConvertFromDecimal(int dis, long long int num){
+    long long int ret = 0;
+    long long int pow = 1;
+    while(num){
+        ret += (num % dis) * pow;
+        pow *= 10;
+        num /= dis;
+    }
+    return ret;
+}
---
+++ 06 - Review/02 - Projects/01 - Conversion Between Two Numbers Systems/ConvertNumberSystem.h
@@ -0,0 +1,8 @@
+#ifndef CONVERT_NUMBER_SYSTEM
+#define CONVERT_NUMBER_SYSTEM
+
+long long int ConvertNumSys(int src, int dis, long long int num);
+long long int ConvertToDecimal(int src, long long int num);
+long long int ConvertFromDecimal(int dis, long long int num);
+
+#endif
