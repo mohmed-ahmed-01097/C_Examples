5ffdff8a51d0643d3f8105b372b8f489831d6445
mohmed-ahmed-01097 <mohmedahmed01097@gmail.com>
2023-03-02 02:25:35 +0200

    update Data type header file

--- 06 - Review/02 - Projects/01 - Data Types/LBTY_CFG.h
+++ 06 - Review/02 - Projects/01 - Data Types/LBTY_CFG.h
@@ -1,7 +1,7 @@
 /********************************************************/
 /* Author      : MAAM					*/
-/* Version     : v01					*/
-/* date        : 03/02/2023				*/
+/* Version     : v02					*/
+/* date        : 02/03/2023				*/
 /********************************************************/
 
 #ifndef LBTY_CFG_H
@@ -17,7 +17,19 @@ typedef signed   short int      s16;
 typedef signed   long  int      s32;
 typedef signed   long long int  s64;
 
-typedef float                   f32;
-typedef double                  f64;
+typedef float        		f32;
+typedef double          	f64;
+
+#define MAX_U8		0xFFU
+#define MAX_S8		0x7F
+#define MIN_S8		0x80
+
+#define MAX_U16		0xFFFFU
+#define MAX_S16		0x7FFF
+#define MIN_S16		0x8000
+
+#define MAX_U32		0xFFFFFFFFU
+#define MAX_S32		0x7FFFFFFF
+#define MIN_S32		0x80000000
 
 #endif // LBTY_CFG_H
