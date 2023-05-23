4957e37c29df2ba047bbf3246593c61178e9b8c2
mohmed-ahmed-01097 <mohmedahmed01097@gmail.com>
2023-05-23 17:42:01 +0300

    update IMT C Pool

--- 06 - Review/03 - IMT C Pool/05 - Functions/04 - STD functions/098_NumLogX.c
+++ 06 - Review/03 - IMT C Pool/05 - Functions/04 - STD functions/098_NumLogX.c
@@ -1,23 +1,43 @@
 #include <stdio.h>
 
-float log(float, int);
+#define ERROR_TOL   0.000001
+
+float ln(float);
+float logx(float, int);
 
 void main(void){
     float num;
-    int x;
+    int base;
 
-    printf("Enter a float number : \n");
+    printf("Enter a float number : ");
     fflush(stdout); fflush(stdin);
     scanf("%f", &num);
 
-    printf("Enter a float number : \n");
+    printf("Enter a float number : ");
     fflush(stdout); fflush(stdin);
-    scanf("%i", &x);
+    scanf("%i", &base);
 
-    printf("[%f]Log[%i] = [%f]\n", num, x, log(num, x));
-
+    printf("[%f]Log[%i] = [%f]\n", num, base, logx(num, base));
 }
 
-float log(float num, int x){
-    return 0.0f;
+float ln(float num){
+    if (num <= 0) {
+        return 0; // or any desired error handling for non-positive values
+    }
+
+    double ret = 0.0f;
+    double step = 1.0f;
+
+    step = (num - 1) / (num + 1);
+    const double x = step * step;
+    for(int i = 1; (step>ERROR_TOL) ; i++){
+        ret += step / (2 * i - 1);
+        step *= x;
+        //printf("%i\t(%f) = [%f]\n", i, step, ret);
+    }
+    return (float)(ret * 2);
+}
+float logx(float num, int base){
+    //logₐ(x) = ln(x) / ln(a)
+    return (float)ln(num)/ln((float)base);
 }
--- 06 - Review/03 - IMT C Pool/05 - Functions/04 - STD functions/099_SinCosTan.c
+++ 06 - Review/03 - IMT C Pool/05 - Functions/04 - STD functions/099_SinCosTan.c
@@ -1,23 +1,61 @@
 #include <stdio.h>
+#include <math.h>
 
-float log(float, int);
+#define MAX_LOOP    10
+#define PI          3.14159265f
+#define ERROR_TOL   0.0000001
+
+float Sin(float);
+float Cos(float);
+float Tan(float);
 
 void main(void){
     float num;
-    int x;
 
-    printf("Enter a float number : \n");
+    printf("Enter a double degree : ");
     fflush(stdout); fflush(stdin);
     scanf("%f", &num);
 
-    printf("Enter a float number : \n");
-    fflush(stdout); fflush(stdin);
-    scanf("%i", &x);
-
-    printf("[%f]Log[%i] = [%f]\n", num, x, log(num, x));
-
+    printf("Sin(%f) = [%f]\t[%f]\n", num, Sin(num), sin(num * PI / 180.0));
+    printf("Cos(%f) = [%f]\t[%f]\n", num, Cos(num), cos(num * PI / 180.0));
+    printf("Tan(%f) = [%f]\t[%f]\n", num, Tan(num), tan(num * PI / 180.0));
 }
 
-float log(float num, int x){
-    return 0.0f;
+float Sin(float degree){
+    double ret = 0.0f;
+    double step = 1.0f;
+
+    degree = degree * PI / 180.0;
+    const double x = degree * degree;
+    step = degree;
+    for(int i = 1; i<=MAX_LOOP ; i++){
+        ret += step;
+        step *= (-x/((2 * i) * (2 * i + 1)));
+        //printf("(%f) = [%f]\n", step, ret);
+        if(step>0 ? (step<ERROR_TOL) : (-step<ERROR_TOL)){
+            break;
+        }
+    }
+    return (float)ret;
+}
+
+float Cos(float degree){
+    double ret = 0.0f;
+    double step = 1.0f;
+
+    degree = degree * PI / 180.0;
+    const double x = degree * degree;
+    for(int i = 1; i<=MAX_LOOP ; i++){
+        ret += step;
+        step *= (-x/((2 * i) * (2 * i - 1)));
+        //printf("(%f) = [%f]\n", step, ret);
+        if(step>0 ? (step<ERROR_TOL) : (-step<ERROR_TOL)){
+            break;
+        }
+    }
+    return (float)ret;
+}
+
+float Tan(float degree){
+    return (float)Sin(degree)/Cos(degree);
 }
